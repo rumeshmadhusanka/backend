83b186cbd68d7542f26d4ff7e1c6ff806a7404a3
Rumesh Madhusanka <32504465+rumeshmadhusanka@users.noreply.github.com>
2019-03-05 20:07:37 +0530

    init

--- index.php
+++ index.php
@@ -4,9 +4,12 @@ require_once 'config.php';
 
 $connection = new PDO($dsn, $username, $password, $options);
 $name=$_POST['name'];
-$sql = "SELECT * FROM users where firstname = '$name'";
+$password=$_POST['password'];
+$passHash=hash("sha512",$password);
+$sql = "SELECT * FROM users where firstname = '$name' and password='$passHash'";
 $stmt=$connection->prepare($sql);
 $stmt->execute();
 $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
 $jsonRes=json_encode($result);
 echo $jsonRes;
+
