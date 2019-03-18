cebc53bb6ac336c95f22e7afa1b056a3c6c3e585
rumesh <rumesh.m.s16@gmail.com>
2019-03-18 21:39:33 +0530

    adduser php

--- userRegister.php
+++ userRegister.php
@@ -2,25 +2,13 @@
 require_once 'common.php';
 require_once 'config.php';
 
-//fake data from frontend
-$_POST['username']="pqr";
-$_POST['firstname']="firstname";
-$_POST['lastname']="last";
-$_POST['password']="password";
-$_POST['email']="email";
-$_POST['age']="24";
-$_POST['location']='2.675684';
-
 $connection = new PDO($dsn, $username, $password, $options);
-$username=$_POST['username'];
-$firstname=$_POST['firstname'];
-$lastname=$_POST['lastname'];
-$password=$_POST['password'];
-$email=$_POST['email'];
-$age=$_POST['age'];
-$location=$_POST['location'];
+$username=$_POST['reg_username'];
+$telephone=$_POST['reg_telephone'];
+$password=$_POST['reg_password'];
+$email=$_POST['reg_email'];
 $passHash=hash("sha512",$password);
-$sql = "insert into users (username, firstname, lastname, password, email, age, location)
-VALUES ('$username','$firstname','$lastname','$passHash','$email','$age','$location')";
+$sql = "insert into user (u_name, u_tele, u_password, u_email)
+VALUES ('$username','$telephone','$passHash','$email')";
 $stmt=$connection->prepare($sql);
 $stmt->execute();
