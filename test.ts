fe69c5dd6ef80a887465560b82f7fb82cd0ab696
rumesh <rumesh.m.s16@gmail.com>
2019-03-27 15:50:21 +0530

    search box- not working

--- test.ts
+++ test.ts
@@ -1,12 +1,12 @@
-$(document).ready(function () {
-    $("#ajaxCall").click(getData);
-    var inp = $("#b");
+$(function () {
+    $("#ajaxCall").on("click",getData);
+    let inp = $("#b");
     function getData() {
         console.log("Bjbegan");
         $.ajax({
             url: "getInfo.php",
             type: "GET",
-            crossDomain: true,
+            data:{name: 'susan'},
             success: function (data) {
                 console.log("fbfgn");
                 inp.append(data);
@@ -16,4 +16,29 @@ $(document).ready(function () {
             }
         });
     }
+
 });
+
+$(function () {
+    console.log("First line");
+    let searchBox= $("#search");
+        searchBox.on("onkeypress",search);
+    let input=<string>(searchBox.val());
+    let resultDisplay=$("#searchResult");
+    function search() {
+        $.ajax({
+            url:'search.php',
+            type: 'GET',
+            data: {keyword: input},
+            success: function (data) {
+                console.log("Executing");
+                resultDisplay.append(data);
+            },
+            error: function (data) {
+                console.log("Error");
+            }
+        })
+    }
+});
+
+
