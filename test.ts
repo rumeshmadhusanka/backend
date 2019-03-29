1585d46473ccbd1d506d47a1e616aef3354d0b61
rumesh <rumesh.m.s16@gmail.com>
2019-03-29 19:01:15 +0530

    improved search box

--- test.ts
+++ test.ts
@@ -1,28 +1,29 @@
 $(function () {
     $("#ajaxCall").on("click",getData);
-    let inp = $("#b");
-    function getData() {
-        console.log("Bjbegan");
-        $.ajax({
-            url: "getInfo.php",
-            type: "GET",
-            data:{name: 'susan'},
-            success: function (data) {
-                console.log("fbfgn");
-                inp.append(data);
-            },
-            error: function (data) {
-                console.log("Error");
-            }
-        });
-    }
 
 });
+
+function getData() {
+    let inp = $("#b");
+    console.log("Bjbegan");
+    $.ajax({
+        url: "getInfo.php",
+        type: "GET",
+        data:{name: 'susan'},
+        success: function (data) {
+            console.log("fbfgn");
+            inp.append(data);
+        },
+        error: function (data) {
+            console.log("Error");
+        }
+    });
+}
 
 $(function () {
     console.log("First line");
     let searchBox= $("#search");
-        searchBox.on("onkeypress",search);
+        searchBox.on("keyup",search);
     let input=<string>(searchBox.val());
     let resultDisplay=$("#searchResult");
     function search() {
@@ -30,8 +31,13 @@ $(function () {
             url:'search.php',
             type: 'GET',
             data: {keyword: input},
+            beforeSend:function () {
+                console.log(input+"vfbv");
+
+            },
             success: function (data) {
                 console.log("Executing");
+                resultDisplay.empty();
                 resultDisplay.append(data);
             },
             error: function (data) {
