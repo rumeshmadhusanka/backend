34db092177da6b522a6adec11d30750e311abe59
rumesh <rumesh.m.s16@gmail.com>
2019-05-04 21:45:36 +0530

    final

--- user/userUpdateLocation.php
+++ user/userUpdateLocation.php
@@ -1,13 +1,23 @@
-<?php //call this script to put data to the database via a ajax request
+<?php //call this script to put data to the database via a ajax GET request
 //update users location every 2 seconds via calling this script.
+//Echos 'SUCCESS' on success
+//Adds the session var 'current_location' (array)
 session_start();
 header('Access-Control-Allow-Origin: *');
-require_once 'common/Database.php';
+require_once '../common/Database.php';
+require_once '../common/Utilities.php';
+
+Utilities::verifyLogIn("USER");
+
+////Test Data
+//$_GET['u_id']=1;
+//$_GET['latitude']=51.507351;
+//$_GET['longitude']=-0.127758;
 
 //get data
-$userId=1;//$_GET['u_id'];
-$latitude=51.507351;//$_GET['latitude'];
-$longitude=-0.127758;//$_GET['longitude'];
+$userId=$_GET['u_id'];
+$latitude=$_GET['latitude'];
+$longitude=$_GET['longitude'];
 
 //DB
 try {
@@ -19,8 +29,9 @@ try {
 //display
 if($result!=null){
     $_SESSION['current_location']=array('latitude'=>$latitude,'longitude'=>$longitude);
-    echo json_encode($_SESSION['current_location']);
+    //echo json_encode($_SESSION['current_location']);
     //echo $_SESSION['current_location']['latitude'];
+    echo 'SUCCESS';
 }else{
     echo 'FAILED';
 }
