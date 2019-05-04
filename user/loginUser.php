fefdddb6efe914f9859e1f4cd1a0226652127924
rumesh <rumesh.m.s16@gmail.com>
2019-05-04 21:17:40 +0530

    final

--- user/loginUser.php
+++ user/loginUser.php
@@ -3,27 +3,31 @@ session_start();
 header('Access-Control-Allow-Origin: *');
 require_once '../common/Database.php';
 require_once '../common/DataType.php';
+require_once '../common/Utilities.php';
 
 //get data
-$name=$_POST['log_username'];
-$password=$_POST['log_password'];
+$name="susan";//$_POST['log_username'];
+$password="password";//$_POST['log_password'];
 
 //validate and prepare
 $user=new UserName($name);
 $user->validate();
-$passHash=hash("sha512",$password);
+$passHash=Utilities::encrypt($password);
 
 //DB
 try {
     $result = Database::read("user", 'u_name = :name and u_password= :pass',
-        array(':name' => $userName->getValue(), ':pass' => $passHash), "u_id,u_name,u_email,u_profile_pic");
+        array(':name' => $user->getValue(), ':pass' => $passHash), "u_id,u_name,u_email,u_profile_pic");
 }catch (Error $e){
     echo 'Could not connect to database';
 }
 //display
 if($result!=null){
     $_SESSION['user']=$result;
-    echo json_encode($_SESSION['user']);
+    $_SESSION['u_id']=$result[0]['u_id'];
+//    echo json_encode($_SESSION['u_id']);
+//    echo json_encode($_SESSION['user']);
+
 }else{
     echo 'FAILED';
 }
