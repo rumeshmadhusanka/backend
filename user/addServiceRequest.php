adb06b2755773392d8233ffebc5b601fefadf30b
rumesh <rumesh.m.s16@gmail.com>
2019-05-04 21:37:35 +0530

    finaL

--- user/addServiceRequest.php
+++ user/addServiceRequest.php
@@ -3,26 +3,36 @@ session_start();
 header('Access-Control-Allow-Origin: *');
 require_once '../common/Database.php';
 require_once '../common/DataType.php';
+require_once '../common/Utilities.php';
+
+////Test data
+//$_SESSION['u_id']=1;
+//$_GET['service_id']=8;
+//$_GET['r_description']="description";
+//$_GET['r_latitude']="3.3434";
+//$_GET['r_longitude']="3.676";
+
+Utilities::verifyLogIn("USER");
 
 //get data-------------------------------------------------------------
-$serviceId = 8;//$_GET['service_id'];
-$description = "description";//$_GET['r_description'];
-$latitude = "3.3434";//$_GET['r_latitude'];
-$longitude = "3.676";//$_GET['r_longitude'];
-$uId = 1;//$_SESSION['u_id'];
+$serviceId = $_GET['service_id'];
+$description = $_GET['r_description'];
+$latitude = $_GET['r_latitude'];
+$longitude = $_GET['r_longitude'];
+$uId = $_SESSION['u_id'];
 
-//find s_id for service id
-$result=Database::read("service","service_id = :s",array(':s'=>$serviceId),"s_id");
-$s_id=$result[0]['s_id'];
 //DB
 try {
+    //find s_id for service id
+    $result = Database::read("service", "service_id = :s", array(':s' => $serviceId), "s_id");
+    $s_id = $result[0]['s_id'];
+
     $result = Database::insert("service_request",
-        array('u_id', 's_id', 'service_id', 'r_description','r_latitude','r_longitude')
-        , array($uId, $s_id, $serviceId, $description,$latitude,$longitude));
+        array('u_id', 's_id', 'service_id', 'r_description', 'r_latitude', 'r_longitude')
+        , array($uId, $s_id, $serviceId, $description, $latitude, $longitude));
 } catch (Error $e) {
     echo 'Could not connect to database';
     die();
 }
-//login display
 
 echo 'SUCCESS';
