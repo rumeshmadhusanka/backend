809fedcb4f1ad3679e45d1fe3bde987888406a3a
rumesh <rumesh.m.s16@gmail.com>
2019-05-04 23:53:29 +0530

    init

---
+++ user/userGetProfileInfo.php
@@ -0,0 +1,21 @@
+<?php
+//get the data from web page via ajax.
+session_start();
+header('Access-Control-Allow-Origin: *');
+require_once '../common/Database.php';
+require_once '../common/DataType.php';
+require_once '../common/Utilities.php';
+
+//verify login
+$_SESSION['u_id'] = 1;//------------------------------
+Utilities::verifyLogIn("USER");
+$uId = $_SESSION['u_id'];
+
+//DB
+try {
+    $result = Database::read("user", 'u_id = :uid',
+        array(':uid' => $uId), "u_id,u_name,u_tele,u_email,u_profile_pic");
+} catch (Error $e) {
+    echo 'FAILED';
+}
+echo json_encode($result);
