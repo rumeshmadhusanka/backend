95f934938b48d5a1b3ac69305de700dd5b65d5c1
rumesh <rumesh.m.s16@gmail.com>
2019-05-08 20:42:22 +0530

    partly done

---
+++ user/userProfileUpdate.ts
@@ -0,0 +1,132 @@
+$(function () {
+    loadDataFromServer();
+    $("#userName").on("keyup", checkUserName);
+    $("#tele").on("keyup", checkTele);
+    $("#email").on("keyup", checkEmail);
+    $("#profilePicUpload").on("change", checkProfilePic);
+    $("#password2").on("keyup", checkPasswordEqual);
+    $("#upload").on("click",uploadProfilePic);
+
+});
+
+function loadDataFromServer(): boolean {
+    let success: boolean = false;
+    $.ajax({
+        url: "userGetProfileInfo.php", method: "GET"
+    }).then(function (dataEx) {
+        let data = JSON.parse(dataEx);
+        let person = data[0];
+        console.log(person);
+        // @ts-ignore
+        document.getElementById("userName").value = (person.u_name).toString();
+        // @ts-ignore
+        document.getElementById("tele").value = (person.u_tele).toString();
+        // @ts-ignore
+        document.getElementById("email").value = (person.u_email).toString();
+        // @ts-ignore
+        $('#profilePic').attr('src', "../" + person.u_profile_pic);
+        success = true;
+    });
+    return success;
+
+}
+
+function checkUserName(): boolean {
+    // @ts-ignore
+    let name = document.getElementById("userName").value;
+    // @ts-ignore
+    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
+    //TODO validate
+    return true;
+}
+
+function checkTele(): boolean {
+    // @ts-ignore
+    let name = document.getElementById("tele").value;
+    // @ts-ignore
+    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
+    //TODO validate
+    return true;
+
+}
+
+function checkEmail(): boolean {
+    // @ts-ignore
+    let name = document.getElementById("email").value;
+    // @ts-ignore
+    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
+    //TODO validate
+    return true;
+
+}
+
+function checkProfilePic(): boolean {
+    // @ts-ignore
+    let name = document.getElementById("profilePic").value;
+    // @ts-ignore
+    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
+    //TODO validate
+    return true;
+}
+
+function checkPasswordEqual(): boolean {
+    console.log("pwchk");
+    // @ts-ignore
+    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
+    // @ts-ignore
+    let p1 = document.getElementById("password1").value;
+    // @ts-ignore
+    let p2 = document.getElementById("password2").value;
+    if (p1 != p2) {
+        // @ts-ignore
+        document.getElementById("alertBox").setAttribute("style", "visibility: visible");
+        // @ts-ignore
+        document.getElementById("alertText").innerHTML = ("Passwords does not match");
+        console.log("!match");
+        return false;
+    } else {
+        // @ts-ignore
+        document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
+        return true;
+    }
+}
+
+function uploadData() {
+    let userName = (<HTMLInputElement>document.getElementById("userName")).value;
+    let password = (<HTMLInputElement>document.getElementById("password1")).value;
+    let email = (<HTMLInputElement>document.getElementById("email")).value;
+    let tele = (<HTMLInputElement>document.getElementById("tele")).value;
+    if (checkUserName() && checkEmail() && checkTele() && checkProfilePic() && checkPasswordEqual()) {
+        // @ts-ignore
+        if (document.getElementById("password2").value.toString() == "11111122333") {
+            password = "";//if the dummy password has not changed, send null
+        }
+        $.ajax({
+            url: "updateUser.php", method: "POST", data: {
+                up_username: userName,
+                up_password: password,
+                up_email: email,
+                up_telephone: tele,
+            }
+        }).then(function (data) {
+            console.log("done in ajax");
+            if (data == "SUCCESS") {
+                alert("Success");
+            } else {
+                alert("Failed ")
+            }
+        }, function () {
+            console.log("failed in fail filter")
+            alert("Failed in fail filter");
+        })
+    } else {
+        alert("Invalid data");
+        console.log("invalid data");
+    }
+//todo replace the alerts with something proper
+
+}
+//todo upload profile picture part
+function uploadProfilePic() {
+
+}
