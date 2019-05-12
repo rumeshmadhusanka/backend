c57c4c82aeb90c184d89fc4a2450268db8397180
rumesh <rumesh.m.s16@gmail.com>
2019-05-12 10:13:28 +0530

    done

--- serviceCenter/getServiceRequests.php
+++ serviceCenter/getServiceRequests.php
@@ -3,9 +3,11 @@ session_start();
 header('Access-Control-Allow-Origin: *');
 require_once '../common/Database.php';
 require_once '../common/DataType.php';
-
+require_once '../common/Utilities.php';
+$_SESSION['s_id']=1;
+Utilities::verifyLogIn("SERVICE_CENTER");
 //get data-------------------------------------------------------------
-$sId = $_SESSION['s_id'];
+$sId = $_SESSION['s_id']=1;
 $status = $_GET['status'];//////////PENDING,DONE,CANCELLED
 
 //find in db
