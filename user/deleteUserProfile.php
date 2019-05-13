13e15fb4c2eba3c129f54df55153ec2e30e4d190
rumesh <rumesh.m.s16@gmail.com>
2019-05-13 10:35:15 +0530

    working

---
+++ user/deleteUserProfile.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+header('Access-Control-Allow-Origin: *');
+header('Content-type:JSON');
+require_once '../common/Database.php';
+require_once '../common/DataType.php';
+require_once '../common/Utilities.php';
+
+$_SESSION['u_id'];
+Utilities::verifyLogIn("USER");
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$passHash = Utilities::encrypt($password);
+
+try {
+    $availability = Database::read("user", "u_id = :uid AND u_email = :email AND u_password = :pass",
+        array(':uid'=>$_SESSION['u_id'],':email' => $email, ':pass' => $passHash));
+    if (json_encode($availability)!= "[]" ) {
+        Database::delete("user", "u_email = :email AND u_password = :pass",
+            array(':email' => $email, ':pass' => $passHash));
+        echo "SUCCESS";
+    } else {
+        echo "WRONG USERNAME OR PASSWORD";
+    }
+} catch (Error $e) {
+    echo "ERROR";
+}
