010a0d0ad8b59ee9661f0e28a71b3f47c702c24a
rumesh <rumesh.m.s16@gmail.com>
2019-05-13 10:35:42 +0530

    fromatted date time

--- user/getReqDetailsCombined.php
+++ user/getReqDetailsCombined.php
@@ -12,14 +12,15 @@ $sql = "select service.service_name,
        service_request.r_description,
        service_station.s_name,
        service_station.s_city,
-       service_request.r_submit_time,
+       DATE_FORMAT(service_request.r_submit_time,'%e/%m/%Y %r')
+           as r_submit_time,
        service.cost,
        service_request.r_status,
        service_station.s_telephone
 from service_request
-         left join service_station on
+         inner join service_station on
     service_request.s_id = service_station.s_id
-         left join service on
+         inner join service on
     service_request.service_id = service.service_id
 where service_request.u_id = :uid";
 try {
