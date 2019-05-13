4c33194d0f21078f71de7c3e0494b7fad7bab46c
rumesh <rumesh.m.s16@gmail.com>
2019-05-13 08:44:33 +0530

    test php

--- test.php
+++ test.php
@@ -1,8 +1,13 @@
 <?php
-require_once 'Database.php';
-//----------------------------------
-echo json_encode(Database::read("user",'u_name = :name'
-    ,array(':name'=>'susan'),"*"));
+//session_start();
+require_once 'common/Database.php';
+//echo json_encode($_SESSION);
+//if (!isset($_SESSION['u_name'])){
+//    die("Not logged in");
+//}
+////----------------------------------
+//echo json_encode(Database::read("user",'u_name = :name'
+//    ,array(':name'=>'susan'),"*"));
 //---------------------------------
 //echo Database::insert('user',array('u_email','u_name'),array('nyhu','vytybufyhbhy'));
 //-----------------------------------
@@ -11,3 +16,14 @@ echo json_encode(Database::read("user",'u_name = :name'
 //    array('u_profile_pic'=>$newTarget),
 //    'u_name= :name',
 //    array(':name'=>'susan'));
+//-------------------------------------------
+//$serviceId="1";
+////DB
+//$result=Database::read("service","service_id = :s",array(':s'=>$serviceId),"s_id");
+////DB
+//echo $result[0]['s_id'];
+//-------------------------------------------
+$i=array("I"=>"uom");
+$j=array("You"=>"uop");
+echo json_encode(array_merge($i,$j));
+header("location: index.html");
