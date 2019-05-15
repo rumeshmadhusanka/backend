cf59696cacdd7a844188293d366537c5daa3bc86
rumesh <rumesh.m.s16@gmail.com>
2019-05-15 21:19:26 +0530

    main func working

--- serviceStation/servicePage.ts
+++ serviceStation/servicePage.ts
@@ -1,35 +1,97 @@
-let row = `<tr>
-            <td>RequestId</td>
-            <td>ServiceId</td>
-            <td>Description</td>
-            <td>Location</td>
-            <td>Status</td>
-            <td>Requested time</td>
-            <td>Action</td>
+class Observer {
+    //html page
+    private readonly work: any;
+
+    constructor(myFunc: any) {
+        this.work = myFunc;
+    }
+
+    public receiveMessage(data: any) {
+        this.work(data);
+    }
+
+}
+
+class Observable {
+    //db
+    private readonly endPoint: string;
+    //private readonly params: object;//like {'a': 25,'b':56}
+    private observers: Observer[];
+
+
+    constructor(endPoint: string) {
+        this.endPoint = endPoint;
+        this.observers = Array();
+
+    }
+
+    public addObserver(ob: Observer): void {
+        this.observers.push(ob);
+    }
+
+    private sendMessage(data: any): void {
+        for (let i = 0; i < this.observers.length; i++) {
+            this.observers[i].receiveMessage(data);
+        }
+    }
+
+    public work(params: object) {
+        let form = new FormData();
+        //console.log(params);
+        for (const [key, value] of Object.entries(params)) {
+            form.append(key, value);
+        }
+        let response = fetch(this.endPoint, {
+            method: "POST",
+            body: form
+        });
+        let promise = response.then(function (data) {
+            //console.log(data);
+            return data.json();
+        });
+        promise.then((msg) => {
+            console.log(msg);
+            let reqId = "";
+            let cus = "";
+            let serId = "";
+            let serName = "";
+            let des = "";
+            let status = "";
+            let reqTime = "";
+            let send=Array();
+            for (let i = 0; i < msg.length; i++) {
+                reqId = msg[i].r_id;
+                serId = msg[i].service_id;
+                cus = msg[i].u_name;
+                serName = msg[i].service_name;
+                des = msg[i].r_description;
+                status = msg[i].r_status;
+                reqTime = msg[i].r_submit_time;
+                let template = `<tr>
+            <td>${reqId}</td>
+            <td>${serId}</td>
+            <td>${cus}</td>
+            <td>${serName}</td>
+            <td>${des}</td>
+            <td>${status}</td>
+            <td>${reqTime}</td>
+            <td>
+            <input class="btn btn-blue-grey" type="button" value="Mark as Done" onclick="doneServiceReq(${reqId})">
+            <input class="btn btn-outline-danger" type="button" value="Cancel Request" onclick="cancelServiceReq(${reqId})">
+            </td>
         </tr>`;
+                send.push(template);
+            }
+            this.sendMessage(send);
 
-// class Iterator1 {
-//     private readonly collection: [];
-//     private index: number;
-//
-//     public constructor(collection: []) {
-//         this.collection = collection;
-//         this.index = 0;
-//     }
-//
-//     public next() {
-//         let current = this.collection[this.index];
-//         this.index += 1;
-//         return current;
-//     }
-// }
+        });
+    }
 
+
+}
 
 $(function () {
-    loadDoneReq();
-    loadPendingReq();
-    loadMyServices();
-
+    main();
 });
 
 async function loadDoneReq() {
@@ -85,24 +147,41 @@ async function loadMyServices() {
     }
 }
 
-async function changeServiceStatus() {
+async function cancelServiceReq(rId: number) {
     //
     let form = new FormData();
     form.append("select", "UPDATE_REQUEST_STATUS");
-    form.append("r_id",this.);
-    form.append("status",);
+    form.append("r_id", rId.toString());//-------------
+    form.append("status", "CANCELLED");//-------------
     let res = await fetch("ServiceStationAll.php", {
         method: "POST",
         body: form
     });
-    let requests = await res.text();
-
+    let text = await res.text();
+    if (text == "SUCCESS"){
+        alert(`Request ${rId} marked as cancelled`);//todo replace alert
+    }
 }
 
-async  function showTotalSalesStatus() {
+async function doneServiceReq(rId: number) {
+    let form = new FormData();
+    form.append("select", "UPDATE_REQUEST_STATUS");
+    form.append("r_id", rId.toString());//-------------
+    form.append("status", "DONE");//-------------
+    let res = await fetch("ServiceStationAll.php", {
+        method: "POST",
+        body: form
+    });
+    let text = await res.text();
+    if (text == "SUCCESS"){
+        alert(`Request ${rId} marked as done`);//todo replace alert
+    }
+}
+
+async function showTotalSalesStatus() {
     let form = new FormData();
     form.append("select", "SHOW_SALES_STATUS");
-    form.append("param","GET_TOTAL");
+    form.append("param", "GET_TOTAL");
     let res = await fetch("ServiceStationAll.php", {
         method: "POST",
         body: form
@@ -111,10 +190,11 @@ async  function showTotalSalesStatus() {
     response[0].total;//todo display answer
 
 }
+
 async function showTopCustomers() {
     let form = new FormData();
     form.append("select", "TOP_CUSTOMERS");
-    form.append("param","GET_TOTAL");
+    form.append("param", "GET_TOTAL");
     let res = await fetch("ServiceStationAll.php", {
         method: "POST",
         body: form
@@ -124,3 +204,62 @@ async function showTopCustomers() {
         console.log(response[i]);//todo use iterator
     }
 }
+
+function main() {
+
+    let pendingObserverFunc = function (data: any) {
+
+        let pendingTable = <HTMLElement>document.getElementById("pendingTable");
+        pendingTable.innerHTML=`<tr>
+            <td>RequestId</td>
+            <td>ServiceId</td>
+            <td>Customer name</td>
+            <td>Service Name</td>
+            <td>Description</td>
+            <td>Status</td>
+            <td>Requested time</td>
+            <td>Action</td>
+        </tr>`;
+        for (let i = 0; i < data.length; i++) {
+            pendingTable.innerHTML+=data[i];
+        }
+
+    };
+    let a1 = new Observer(pendingObserverFunc);
+    let params = {"select": "GET_SERVICE_REQUESTS", "status": "PENDING"};
+    let pendingObservable = new Observable("ServiceStationAll.php");
+    pendingObservable.addObserver(a1);
+    // pendingObservable.work(params);
+    pendingObservable.work(params);
+    setInterval(() => {
+        pendingObservable.work(params)
+    }, 1000);
+
+    let doneObserverFunc = function (data: any) {
+
+        let pendingTable = <HTMLElement>document.getElementById("doneTable");
+        pendingTable.innerHTML=`<tr>
+            <td>RequestId</td>
+            <td>ServiceId</td>
+            <td>Customer name</td>
+            <td>Service Name</td>
+            <td>Description</td>
+            <td>Status</td>
+            <td>Requested time</td>
+            <td>Action</td>
+        </tr>`;
+        for (let i = 0; i < data.length; i++) {
+            pendingTable.innerHTML+=data[i];
+        }
+
+    };
+    let a2=new Observer(doneObserverFunc);
+    let params2 = {"select": "GET_SERVICE_REQUESTS", "status": "DONE"};
+    let doneObservable =new Observable("ServiceStationAll.php");
+    doneObservable.addObserver(a2);
+    doneObservable.work(params2);
+    setInterval(() => {
+        pendingObservable.work(params)
+    }, 1000);
+}
+
