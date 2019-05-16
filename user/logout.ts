7b0c202dfaa27e81bd69c04c8e1953d438de586f
rumesh <rumesh.m.s16@gmail.com>
2019-05-16 23:36:55 +0530

    logout ts

---
+++ user/logout.ts
@@ -0,0 +1,15 @@
+function logoutUser() {
+    let form = new FormData();
+    form.append("select", "LOGOUT");
+    let res = fetch("userAll.php", {
+        method: "POST",
+        body: form
+    });
+    res.then(function (data) {
+        window.location.replace("start.html");
+    });
+    res.catch(function () {
+        window.location.replace("start.html");
+    })
+
+}
