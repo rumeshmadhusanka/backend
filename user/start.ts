f493b9ddd53359a4db25551d08f2d9df9be4a828
Rumesh Madhusanka <rumesh.m.s16@gmail.com>
2019-05-27 23:12:14 +0530

    regex 2

--- user/start.ts
+++ user/start.ts
@@ -1,6 +1,6 @@
 $(function () {
     $("#submitLogin").on('click', loginUser);
-    $("#signUpBtn").on('click',signUpUser);
+    $("#signUpBtn").on('click', signUpUser);
 });
 
 async function loginUser() {
@@ -13,19 +13,23 @@ async function loginUser() {
     });
     console.log(response);
     let resText = await response.text();
-    if (resText=="SUCCESS"){
+    if (resText == "SUCCESS") {
         window.location.replace("service page.html");
     }
 }
 
 // @ts-ignore
 function checkUserName(): boolean {
-    // @ts-ignore
-    let name = document.getElementById("userName").value;
-    // @ts-ignore
-    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
-    //TODO validate
-    return true;
+    let term = (<HTMLInputElement>document.getElementById("userName")).value;
+    let pattern = /^[a-z A-Z 0-9]+([_ -])?[a-zA-Z0-9]*$/;
+    let re = new RegExp(pattern);
+    if (re.test(term)) {
+        (<HTMLInputElement>document.getElementById("alertBox")).setAttribute("style", "visibility: hidden");
+        return true;
+    } else {
+        console.log("Invalid");
+        return false;
+    }
 }
 
 
@@ -35,9 +39,9 @@ function checkTele(): boolean {
     let pattern = /^[0-9]{9,10}/;
     let re = new RegExp(pattern);
     if (re.test(term)) {
-    (<HTMLInputElement>document.getElementById("alertBox")).setAttribute("style", "visibility: hidden");
-    return true;
-    }else{
+        (<HTMLInputElement>document.getElementById("alertBox")).setAttribute("style", "visibility: hidden");
+        return true;
+    } else {
         console.log("Invalid");
         return false;
     }
@@ -48,7 +52,7 @@ function checkTele(): boolean {
 // @ts-ignore
 function checkEmail(): boolean {
     let term = (<HTMLInputElement>document.getElementById("email")).value;
-    let pattern =/^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let pattern = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let re = new RegExp(pattern);
     if (re.test(term)) {
         console.log("Email valid");
@@ -63,49 +67,15 @@ function checkEmail(): boolean {
 }
 
 
-// @ts-ignore
-function checkProfilePic(): boolean {
-    // @ts-ignore
-    let name = document.getElementById("profilePic").value;
-    // @ts-ignore
-    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
-    //TODO validate
-    return true;
-}
-
-
-// @ts-ignore
-function checkPasswordEqual(): boolean {
-    console.log("pwchk");
-    // @ts-ignore
-    document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
-    // @ts-ignore
-    let p1 = document.getElementById("password1").value;
-    // @ts-ignore
-    let p2 = document.getElementById("password2").value;
-    if (p1 != p2) {
-        // @ts-ignore
-        document.getElementById("alertBox").setAttribute("style", "visibility: visible");
-        // @ts-ignore
-        document.getElementById("alertText").innerHTML = ("Passwords does not match");
-        console.log("!match");
-        return false;
-    } else {
-        // @ts-ignore
-        document.getElementById("alertBox").setAttribute("style", "visibility: hidden");
-        return true;
-    }
-}
-
 async function signUpUser() {
     let form = new FormData(<HTMLFormElement>document.getElementById("signUpForm"));
-    form.append("select","SIGN_UP");
-    let res = await fetch("userAll.php",{
-        method:"POST",
-        body:form
+    form.append("select", "SIGN_UP");
+    let res = await fetch("userAll.php", {
+        method: "POST",
+        body: form
     });
     let text = await res.text();
-    if (text=="SUCCESS"){
+    if (text == "SUCCESS") {
         alert("Sign up successful. Please login to continue");
     } else {
         alert("Error. Try again");//todo replace alerts
