09ea3c49ca7f198e741c50c97ba5d15349e1d667
Rumesh Madhusanka <rumesh.m.s16@gmail.com>
2019-05-29 18:58:00 +0530

    map loc service

--- serviceStation/mapStation.js
+++ serviceStation/mapStation.js
@@ -1,24 +1,28 @@
-let lat0:any;
-let lng0:any;
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        lat0=position.coords.latitude;
-        lng0=position.coords.longitude;
+//
+// let lat0;
+// let lng0;
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//         lat0=position.coords.latitude;
+//         lng0=position.coords.longitude;
+//
+//     });
+//     // document.write(lat0);
+//     // document.write(lng0);
+// } else {
+//     alert("Geolocation is not supported by this browser.");
+// }
+//
 
-    });
-    // document.write(lat0);
-    // document.write(lng0);
-} else {
-    alert("Geolocation is not supported by this browser.");
-}
 
-getGeolocation();
-getClickLocation();
+
+//getGeolocation();
+
 function getGeolocation() {
     let confirmBtn = document.getElementById('submit');
     let latID = 1;
     let lngID = 1;
-    let map = document.getElementById('map');
+    let map = document.getElementById('mapLoc');
     let lat1 = lat0;
     let lng1 = lng0;
 
@@ -26,14 +30,14 @@ function getGeolocation() {
         navigator.geolocation.getCurrentPosition(function (position) {
             lat1 = position.coords.latitude;
             lng1 = position.coords.longitude;
-            console.log(position.coords);
+            //console.log(position.coords);
+
         });
-        return {lat: lat1, log: lng1};
+        //return {lat: lat1, log: lng1};
     } else {
         alert("Geolocation is not supported by this browser.");
     }
     // Initialize LocationPicker plugin
-    // @ts-ignore
     let lp = new locationPicker(map, {
         setCurrentPosition: true, // You can omit this, defaults to true
         lat: lat1,
@@ -41,19 +45,34 @@ function getGeolocation() {
     }, {
         zoom: 10, // You can set any google map options here, zoom defaults to 15
     });
-
-
-    google.maps.event.addListener(lp.map, 'idle', function (event:any) {
+    google.maps.event.addListener(lp.map, 'idle', function (event) {
         let location = lp.getMarkerPosition();
         latID = location.lat;
         lngID = location.lng;
     });
 }
-function getClickLocation() {
+
+getClickLocation(lat0=-12.3,lng0=131.044);
+function getGeolocation() {
+    var myLatLng = {lat: -25.363, lng: 131.044};
+
+    var map = new google.maps.Map(document.getElementById('mapLoc'), {
+        zoom: 4,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!',
+        draggable:false
+    });
+}
+function getClickLocation(lat0,lng0) {
     let confirmBtn = document.getElementById('submit');
     let latID = 1;
     let lngID = 1;
-    let map = document.getElementById('map');
+    let map = document.getElementById('mapLoc');
     let lat1 = lat0;
     let lng1 = lng0;
 
@@ -67,18 +86,22 @@ function getClickLocation() {
         alert("Geolocation is not supported by this browser.");
     }
     // Initialize LocationPicker plugin
-    // @ts-ignore
     let lp = new locationPicker(map, {
         setCurrentPosition: true, // You can omit this, defaults to true
         lat: lat1,
-        lng: lng1
+        lng: lng1,
     }, {
         zoom: 10, // You can set any google map options here, zoom defaults to 15
     });
-    google.maps.event.addListener(lp.map, 'click', function (event:any) {
+
+
+    google.maps.event.addListener(lp.map, 'idle', function (event) {
         let location = lp.getMarkerPosition();
-        latID = event.latLng.lat();
-        lngID = event.latLng.lng();
+        latID = location.lat;
+        lngID = location.lng;
         console.log(latID + "," + lngID);
+        //todo change with element id
+        document.getElementById("latInp").value = latID;
+        document.getElementById("longInp").value = lngID;
     });
 }
